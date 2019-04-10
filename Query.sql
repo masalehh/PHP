75ef04adb2eafef63896e435123d27c44503d1b4
Md. Abdullah Saleh <ma.saleh007@gmail.com>
2019-04-10 23:44:47 +0600

    Update Query.sql

--- Query.sql
+++ Query.sql
@@ -2,6 +2,6 @@ SELECT con.compaign_id compaignID,
        com.name Name,
        SUM(con.revenue) totalRevenue,
        count(con.lander_id) landerCount
-FROM CONVERSION CON
-JOIN compaign com on(con.compaign_id = com.id)
+FROM conversions CON
+JOIN compaigns com on(con.compaign_id = com.id)
 GROUP BY con.compaign_id
